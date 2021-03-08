22268176429427f248b7b8e39c02994f77115a09
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-03-08 11:27:45 +0700

    � add burger menu

--- app/script/index.js
+++ app/script/index.js
@@ -1,1 +1,9 @@
 console.log('Hello World!');
+
+const burgerMenu =  document.querySelectorAll('.header__menu');
+
+burgerMenu.forEach(btn => {
+  btn.addEventListener('click', e => {
+      btn.classList.toggle('active');
+  });
+});
