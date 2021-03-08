e789273256f0b03266e5b26828852a7370dc284b
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-03-08 15:41:30 +0700

    Added mobile navbar functionality

--- app/script/header.js
+++ app/script/header.js
@@ -1,10 +1,40 @@
 export function Header() {
   const navbar = document.querySelector('.header');
-  const burgerMenu =  document.querySelectorAll('.header__menu');
+  const burger =  document.querySelectorAll('.header__menu');
+  const overlay = document.querySelector('.overlay');
+  const menu = document.querySelector(".header__links")
+  const menuLink = menu.childNodes;
 
-  burgerMenu.forEach(btn => {
+  const burgerClick = () => {
+    navbar.classList.toggle("menu-clicked");
+      if (!overlay.classList.contains('fade-in')) {
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
+        menu.classList.add('fade-in')
+        menu.classList.remove('fade-out')
+      } else {
+        overlay.classList.add('fade-out')
+        overlay.classList.remove('fade-in')
+        menu.classList.add('fade-out')
+        menu.classList.remove('fade-in')
+      }
+  }
+  
+  const menuClick = () => {
+    if (navbar.classList.contains('menu-clicked')) {
+      burger.forEach(btn => btn.classList.toggle('active'))
+      burgerClick()
+    }
+  }
+  
+  menuLink.forEach(link => {
+    link.addEventListener('click', ()=> menuClick())
+  })
+
+  burger.forEach(btn => {
     btn.addEventListener('click', e => {
       btn.classList.toggle('active');
+      burgerClick();
     });
   });
 
