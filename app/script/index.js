9db9cedc2239caf9388212d7b676e1f88f04bb91
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-03-13 18:20:33 +0700

    Form Contact Implementation

--- app/script/index.js
+++ app/script/index.js
@@ -1,10 +1,12 @@
 import { Header } from './Header.js';
 import { Main } from './Main.js';
 import { DarkMode } from './DarkMode.js'
+import { ContactForm } from './ContactForm.js'
 
 window.addEventListener('DOMContentLoaded', (event) => {
   Header();
   DarkMode();
   Main();
+  ContactForm();
 });
 
