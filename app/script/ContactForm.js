85f08a96648d7ea9f3f26744e6cf3f2bce5a1ab8
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-03-16 18:01:13 +0700

    Refactoring  contact js

--- app/script/ContactForm.js
+++ app/script/ContactForm.js
@@ -2,25 +2,25 @@ export function ContactForm() {
   const scriptURL =
     "https://script.google.com/macros/s/AKfycby6jRTrVhNkZn2ipk8E4hsJQy7sr6AxGGqO23Tunk-8iB53DbYMo_VGnZmjN9KRPdiQ/exec";
   const form = document.forms["submit-to-google-sheet"];
-  const submitBtn = document.querySelector('.btn-submit');
+  const submitBtn = document.querySelector(".btn-submit");
 
   const resetButton = () => {
-    submitBtn.classList.remove('-success');
-    submitBtn.classList.remove('-failed');
+    submitBtn.classList.remove("-success");
+    submitBtn.classList.remove("-failed");
     submitBtn.innerHTML = `
       <span class="fas fa-paper-plane"></span>
       Send
     `;
-  }
+  };
 
   const resetForm = () => {
     resetButton();
     form.reset();
-  }
+  };
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    submitBtn.classList.add('-request');
+    submitBtn.classList.add("-request");
     submitBtn.innerHTML = `
       <span>
         <svg>
@@ -31,9 +31,9 @@ export function ContactForm() {
     `;
     fetch(scriptURL, { method: "POST", body: new FormData(form) })
       .then((response) => {
-        console.log("Success!", response)
-        submitBtn.classList.remove('-request')
-        submitBtn.classList.add('-success')
+        console.log("Success!", response);
+        submitBtn.classList.remove("-request");
+        submitBtn.classList.add("-success");
         submitBtn.innerHTML = `
           <span>
             <svg>
@@ -48,9 +48,9 @@ export function ContactForm() {
         setTimeout(resetForm, 2500);
       })
       .catch((error) => {
-        console.error("Error!", error.message)
-        submitBtn.classList.remove('-request')
-        submitBtn.classList.add('-failed')
+        console.error("Error!", error.message);
+        submitBtn.classList.remove("-request");
+        submitBtn.classList.add("-failed");
         submitBtn.innerHTML = `
           <span class="fas fa-exclamation-circle"></span>
           Oops! Try again in few sec..
