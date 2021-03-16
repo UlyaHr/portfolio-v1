ab1e17fee4f7f6bd66c3bbeeb88c681a2943bd90
UlyaHr <ulya.habiburrahman@mail.ugm.ac.id>
2021-03-16 18:03:00 +0700

    Refactoring index js

--- app/script/index.js
+++ app/script/index.js
@@ -1,14 +1,13 @@
-import { Header } from './Header.js';
-import { Main } from './Main.js';
-import { DarkMode } from './DarkMode.js'
-import { ContactForm } from './ContactForm.js'
-import { About } from './About.js';
+import { Header } from "./Header.js";
+import { Main } from "./Main.js";
+import { DarkMode } from "./DarkMode.js";
+import { ContactForm } from "./ContactForm.js";
+import { About } from "./About.js";
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", () => {
   Header();
   DarkMode();
   Main();
   About();
   ContactForm();
 });
-
